8d8f12cab9e9d29582312d7b1d690107bab3f8d2
David Utreras <dutreras369@gmail.com>
2024-07-26 21:37:27 -0400

    actividad formulario

--- modulo 2/sesion 2/js/script.js
+++ modulo 2/sesion 2/js/script.js
@@ -4,8 +4,10 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     var name = document.getElementById('nombre').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('msj').value;
+    var genero = document.getElementById('genero').value;
 
-    if (name && email && message) {
+
+    if (name && email && message && genero ) {
         document.getElementById('response').textContent = '¡Gracias, ' + name + '! Hemos recibido tu mensaje.';
     } else {
         document.getElementById('response').textContent = 'Por favor, completa todos los campos.';
